60d0bf896c0d18a17e08757573cbcba58508a68d
novagenno <novageno@163.com>
2018-01-07 21:01:35 +0800

    fix some bugs

--- train_net.py
+++ train_net.py
@@ -19,7 +19,7 @@ from model_factory import word_factory
 
 
 def parse_args():
-    
+
     parser = argparse.ArgumentParser(description='Train Text Classification model')
     parser.add_argument('--model',help='which model used',default='CNNText',type=str)
     parser.add_argument('--gpus',help='use which gpu to train the model',default='0',type=str)
@@ -28,6 +28,8 @@ def parse_args():
     parser.add_argument('--momentum',help='momentum value for optimizer',default=0.9,type=float)
     parser.add_argument('--wd', type=float, default=0.0001,
                         help='weight decay rate. default is 0.0001.')
+    parser.add_argument('--lr_step',help='learning rate step',default='3,5',type=str)
+    parser.add_argument('--lr_factor',help='learning rate deacy factor',default=0.1,type=float)
     parser.add_argument('--frequency',help='after n bacth output the log',default=50,type=int)
     parser.add_argument('--begin_epoch',help='beigin epoch, set to 0 means no resume',default=0,type=int)
     parser.add_argument('--end_epoch',help='end epoch',default=10,type=int)
@@ -45,7 +47,7 @@ def train_net(args):
     logging.basicConfig()
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    
+
     ctx = [mx.gpu(int(i)) for i in args.gpus.split(',')] if args.gpus else [mx.cpu()]
     config.model = args.model
     config.ctx = ctx
@@ -56,24 +58,25 @@ def train_net(args):
     net = word_factory(config)
     if args.begin_epoch > 0:
         logging.info('resume from epoch {}'.format(args.begin_epoch))
-        net.load_params(model_path + str(args.begin_epoch).rjust(3,'0') + '.params')
+        net.load_params(model_path + '/' + str(args.begin_epoch).rjust(3,'0') + '.params',ctx=ctx)
     else:
         net.collect_params().initialize(init=mx.init.Xavier(),ctx=ctx)
-    
+
     #Data Loader
     data_root = config.data_root
     dataset = os.path.join(data_root,'word_train.h5py')
-    train_data = WordLoader(dataset,args.batch_size,mode='train',shuffle=False)
+    train_data = WordLoader(dataset,args.batch_size,mode='train',shuffle=True)
     val_data = WordLoader(dataset,args.batch_size,mode='val',shuffle=False)
-    
+
     #Optimizer
     lr = args.lr
+    lr_step =  [int(i) for i in args.lr_step.split(',')]
     wd = args.wd
     Trainer = gluon.Trainer(net.collect_params(),'adam',{'learning_rate':lr,'wd':wd})
     #Loss and evluate metirc
     metric = mx.metric.CompositeEvalMetric()
     #loss_metric = LogLoss()
-    metric.add(Accuracy(0.3))
+    metric.add(Accuracy(0.5))
     metric.add(LogLoss())
     Loss = gluon.loss.SigmoidBinaryCrossEntropyLoss(from_sigmoid=True)
     for epoch in range(args.begin_epoch,args.end_epoch):
@@ -100,7 +103,7 @@ def train_net(args):
                 info = parse_log(names,values)
                 logging.info('epoch[{}] batch[{}]'.format(epoch,i) + info )
         names,values = metric.get()
-        info = parse_args(names,values)
+        info = parse_log(names,values)
         logging.info('epoch[{}], Training '.format(epoch) + info)
         #name,val_acc = test_net(net,val_data,ctx)
         #validate
@@ -114,23 +117,16 @@ def train_net(args):
                 output.append(net(x))
             metric.update(vallabel,output)
         names,values = metric.get()
-        info = parse_args(names,values)
+        info = parse_log(names,values)
         logging.info('epoch[{}],Val'.format(epoch) + info)
+        #reset the learning rate
+        if epoch in lr_step:
+            lr = lr * args.lr_factor
+            Trainer.set_learning_rate(lr)
+            logging.info('changing learning rate to {}'.format(lr))
+        #save the model
+        net.collect_params().save(model_path + '/' + str(epoch).rjust(3,'0') + '.params')
 
 if __name__=="__main__":
     args = parse_args()
     train_net(args)
-            
-            
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
